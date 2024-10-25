187126d97d231faf18ae8549a81adec9aa3c9146
CianDickerHughes <g00415413@atu.ie>
2024-10-25 13:43:02 +0000

    Exercise 4

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
+// message route 
 app.get('/', (req, res) => {
     res.send('Welcome to Data Respresentation & Querying');
 });
 
-app.get('/whatever', (req, res) => {
-    res.send('Hello world');
+// message route of name
+app.get('/hello/:fName/:sName', (req, res) => {
+    res.send("Hello "+ req.params.fName +" "+ req.params.sName);
 });
 
+// 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
-
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
 });
